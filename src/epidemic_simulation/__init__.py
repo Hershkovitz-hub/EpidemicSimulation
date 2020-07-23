10d4295a640b4ca7e896ac6674dace74c4c2d842
Gal Hershkovitz <hershkovitz1@mail.tau.ac.il>
2020-07-23 12:34:39 +0300

    made the package pip installable

---
+++ src/epidemic_simulation/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__title__ = "Epidemic Simulation"
+__description__ = "Parse Stuff With Ease"  ## CHANGE
+__url__ = "https://github.com/Hershkovitz-hub/EpidemicSimulation"
+__doc__ = __description__ + " <" + __url__ + ">"
+__author__ = "Will be changed"
+__email__ = "Will be changed@gmail.com"
+__license__ = "MIT"
