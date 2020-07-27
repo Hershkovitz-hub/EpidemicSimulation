5eba1b3213f4bcffc253072a1b4f7f422bd3ac09
Noga_Yair <nogayair23@gmail.com>
2020-07-27 13:27:00 +0300

    testing files for 3 functions in simulation

---
+++ src/epidemic_simulation/testing/test_calc.py
@@ -0,0 +1,28 @@
+from random import randint
+from simulation_manager import SimulationManager
+
+
+states = ["SUSCEPTIBLE", "INFECTIOUS", "REMOVED"]
+def generate_bodies_example(n_bodies: int) -> list:
+    bodies = []
+    for i in range(n_bodies):
+        bodies.append(
+            {
+                "position_x": randint(650, 1270),
+                "position_y": randint(10, 690),
+                "state": states[randint(0, 2)],
+            }
+        )
+    return bodies
+
+def test_calc_len():
+    bodies=generate_bodies_example(20)
+    test=SimulationManager(bodies,12,0.2,6)
+    test.calculate_bodies_to_change()
+    assert len(test.bodies_to_change) == len(set(test.bodies_to_change))
+
+def test_calc_status():
+    bodies=generate_bodies_example(20)
+    test=SimulationManager(bodies,12,0.2,6)
+    test.calculate_bodies_to_change()
+    assert set([body['state'] for body in test.bodies_to_change if body['state']])=='INFECTIOUS'
